0b6555cb2704096987fc9bb2d9ab0096206ae59e
nothingprogram <popawaw@naver.com>
2022-07-17 17:27:34 +0900

    [Greedy] - 결혼식 o(N)

--- src/main/kotlin/greedy/WeddingSeremony.kt
+++ src/main/kotlin/greedy/WeddingSeremony.kt
@@ -1,5 +1,34 @@
 package greedy
 
+import java.lang.System.`in`
+import java.util.*
+
 /**
  * @author jongyunha created on 22. 7. 17.
  */
+
+private fun solution(peopleOfTime: ArrayList<Pair<Int, String>>): Int {
+    var result = Int.MIN_VALUE
+    var count = 0
+
+    val peopleOfTime = peopleOfTime.sortedWith(compareBy({ it.first }, { it.second }))
+
+    for (p in peopleOfTime) {
+        if (p.second.equals("s")) count++
+        else count--
+        result = Math.max(result, count)
+    }
+    return result
+}
+
+fun main() {
+    val scanner = Scanner(`in`)
+    val N = scanner.nextInt()
+
+    val peopleOfTime = ArrayList<Pair<Int, String>>()
+    for (i in 0 until N * 2) {
+        peopleOfTime.add(Pair(scanner.nextInt(), if (i % 2 == 0) "s" else "e"))
+    }
+
+    println(solution(peopleOfTime))
+}
